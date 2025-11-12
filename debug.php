9bfed518966524d734db1087cbcdf73bbc146430
sujaldat <sujaldat@buffalo.edu>
2025-11-12 14:40:21 -0500

    Update debug.php

--- debug.php
+++ debug.php
@@ -111,3 +111,92 @@ exit();
 
 </body>
 </html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function getDB() {
+    $dbhost="10.9.0.6";
+    $dbuser="seed";
+    $dbpass="dees";
+    $dbname="sqllab_users";
+    $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error . "\n");
+    }
+    // set charset to avoid charset related injection issues
+    $conn->set_charset('utf8mb4');
+    return $conn;
+}
+
+$conn = getDB();
+// Using prepared statements to remove SQL injection  possibility
+if ($id > 0) {
+    if ($input_pwd !== '') {
+        // update including password
+        $hashed_pwd = sha1($input_pwd);
+        // update password in session
+        $_SESSION['pwd'] = $hashed_pwd;
+        $stmt = $conn->prepare(
+            "UPDATE credential SET nickname = ?, email = ?,address = ?,PhoneNumber = ?,Password = ?WHERE ID = ?");
+        if ($stmt === false) {
+            error_log("Prepared failed: " . $conn->error);
+            $conn->close();
+            header("Location: unsafe_home.php");
+            exit();
+        }
+        // types: s=string and there are 5 of these, i=integer
+        $stmt->bind_param(
+            "sssssi",$input_nickname,$input_email,
+            $input_address,$input_phonenumber,$hashed_pwd,$id);
+
+    } else {
+        // update without changing password since password change is not requested
+        $stmt = $conn->prepare(
+            "UPDATE credential SET nickname = ?,email = ?,address = ?,PhoneNumber = ? WHERE ID = ?");
+        if ($stmt === false) {
+            error_log("Prepared failed: " . $conn->error);
+            $conn->close();
+            header("Location: unsafe_home.php");
+            exit();
+        }
+
+    $stmt->bind_param("ssssi",$input_nickname,$input_email,$input_address,$input_phonenumber,$id);}
+    $stmt->execute();
+    $stmt->close();
+}
+
+$conn->close();
+header("Location: unsafe_home.php");
+exit();
+?>
+
+</body>
+</html>
